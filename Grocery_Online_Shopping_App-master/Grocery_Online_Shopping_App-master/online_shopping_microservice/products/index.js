8a5b9617127d8c01a71dd01646844fe22daa36e3
DhruvShah <dhruvshah78223@gmail.com>
2022-12-19 11:19:31 +0530

    monlothic project convert to making microservice project

---
+++ Grocery_Online_Shopping_App-master/Grocery_Online_Shopping_App-master/online_shopping_microservice/products/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+
+const app=express();
+
+app.use(express.json());
+
+
+app.use('/',(req,res,next)=>{
+    return res.status(200).json({"msg":"Hello from the Products"});
+});
+
+
+app.listen(8002,()=>{
+    console.log('Products is listening port 8002');
+})
